439332bf142e3a3c2fd635ba3ff1e13e946ced11
beauti21kk <kornkanok_kanchana@cmu.ac.th>
2023-07-11 16:40:44 +0700

    4.2

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -1,7 +1,20 @@
 <template>
-    <div class="hello"></div>
+    <div class="event-class"></div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.event-card{
+    padding: 20px;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid #39495c;
+    margin-bottom: 18px;
+}
+
+.event-card:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
