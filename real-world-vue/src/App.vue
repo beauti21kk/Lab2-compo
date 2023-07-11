a7efb5b477a1d64f49d1cd73e210db60375da851
beauti21kk <kornkanok_kanchana@cmu.ac.th>
2023-07-11 19:16:15 +0700

    5.1

--- real-world-vue/src/App.vue
+++ real-world-vue/src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+      <h1>Events For Good</h1>
     </div>
   </header>
 
